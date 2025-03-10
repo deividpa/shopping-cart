8a48b41bd63ef53a401d77f0b265e67a5fc3a1bc
David Pérez <deividpa022@gmail.com>
2025-03-09 23:21:33 -0500

    feat: add product filtering functionality to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products'
+import { useState } from 'react'
+import Header from './components/Header';
 
 function App() {
 
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  });
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      if (filters.category === 'all') {
+        return product.price >= filters.minPrice;
+      }
+      return product.category === filters.category && product.price >= filters.minPrice;
+    });
+  }
+
+  const filteredProducts = filterProducts(products);
+
+
+
   return (
     <>
-      <h1>Shopping Cart</h1>
-      <Products products={products} />
+      <Header filters={filters} setFilters={setFilters} />
+      <Products products={filteredProducts} />
     </>
     
   )
