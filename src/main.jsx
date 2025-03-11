f974d61f426516b771f4c60791e2a9032defc9b2
David Pérez <deividpa022@gmail.com>
2025-03-11 00:21:00 -0500

    feat: wrap App component with FiltersProvider for context management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FiltersProvider } from './context/filters.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <FiltersProvider>
     <App />
-  </StrictMode>,
+  </FiltersProvider>,
 )
