7f111a5afd406c44df8ae962949e158c97cd5f22
David Pérez <deividpa022@gmail.com>
2025-03-10 23:56:36 -0500

    feat: add Footer component with attribution and styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Hecho con <span role="img" aria-label="corazón">❤️</span> por <a href="https://www.linkedin.com/in/david-perez-aguirre" target="_blank" rel="noreferrer">David Pérez</a></p>
+    </footer>
+  )
+}
+
+export default Footer
