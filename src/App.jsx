180659a5bed83951b512a5d0bfd8e25843e7781b
David Pérez <deividpa022@gmail.com>
2025-03-10 23:56:23 -0500

    feat: integrate custom hook for filters and add Footer component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import { products as initialProducts } from './mocks/products.json'
-import { Products } from './components/Products'
 import { useState } from 'react'
+import useFilters from './hooks/useFilters';
+import { Products } from './components/Products'
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
   const [products] = useState(initialProducts);
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  });
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      if (filters.category === 'all') {
-        return product.price >= filters.minPrice;
-      }
-      return product.category === filters.category && product.price >= filters.minPrice;
-    });
-  }
+  const {filters, filterProducts, setFilters} = useFilters();
 
   const filteredProducts = filterProducts(products);
-
-
 
   return (
     <>
       <Header filters={filters} setFilters={setFilters} />
       <Products products={filteredProducts} />
+      <Footer filters={filters} />
     </>
     
   )
